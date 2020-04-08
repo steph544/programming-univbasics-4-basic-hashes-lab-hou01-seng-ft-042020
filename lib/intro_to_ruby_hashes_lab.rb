3828939f69441a4052ddef2902dde735b45ead59
Stephanie Cheney <steph@scheneyphotography.com>
2020-04-08 16:13:03 +0000

    Done.

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,22 @@
 def new_hash
-  # return an empty hash
+  new_hash={}
+  new_hash
 end
 
 def my_hash
+  new_hash={name: "Stephanie"}
+  new_hash 
   # return a valid hash with any key/value pair of your choice
 end
 
 def pioneer
+  new_hash={name: "Grace Hopper"}
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
 end
 
 
 def id_hash_generator(number)
+  new_hash={id: number}
+  new_hash
   # return a hash with a key :id assigned to the provided number
 end
